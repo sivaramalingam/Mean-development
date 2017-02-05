58362ed01e73dd0b3e6cccfc411de72c340385cb
sivaramalingam <lingam.sivarama@gmail.com>
2017-02-05 21:19:16 +0530

    First commit for mean development

---
+++ server.js
@@ -0,0 +1,8 @@
+
+var express = require('express'),
+    app = express(),
+    serveStatic = require('serve-static');
+
+    app.use(serveStatic(__dirname+"/app")).listen('9000', function(){
+      console.log("Node server running at 9000");
+    });
