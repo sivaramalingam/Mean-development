bc3955488e24771b3014c987956986006e971899
siva <sivaramalingam.r@whatarage.com>
2017-02-06 18:53:26 +0530

    Mean development commit on 07/02/17

--- app/assets/scripts/script.js
+++ app/assets/scripts/script.js
@@ -1,1 +1,14 @@
-var remainderApp = angular.module("remainder",[]);
+(function(){
+  var remainderApp = angular.module("remainder",[]);
+      //login controller
+      remainderApp.controller("login", function($scope){
+
+      });
+    // register controller
+      remainderApp.controller("registeration", function($scope){
+        $scope.submitForm = function(isValid){
+          if(isValid)
+            console.log("form submitted successfully!!")
+        }
+      });
+})();
--- server.js
+++ server.js
@@ -3,14 +3,45 @@ var express = require('express'),
     app = express(),
     serveStatic = require('serve-static');
 
-    app.use(serveStatic(__dirname+"/app"), function(){
-      console.log("Node server running at 9000");
-    });
-    app.listen('9000');
-
-var databaseUrl = "sampledb",
+var databaseUrl = "mongodb://localhost:27017/remainder",
     collections = ["register"],
-    db = require("mongo");
-    db.connect(databaseUrl, collections, function(){
-      console.log("db connected!!!")
+    mongo = require("mongodb"),
+    mongoServer = mongo.Server,
+    mongoDb = mongo.Db,
+    serverd = new mongoServer("localhost",27017,{auto_reconnect: true}),
+    db = new mongoDb('remainder', serverd);
+    db.open(function(err, db){
+      if(!err){
+        console.log('Mongo db connected!!!');
+        var collectionsRef = db.collection("register");
+        //Insert Data into MongoDB
+          // dataVar = ({"userId":"0005", "name":"test5", "lastname":"test", "address":"MDU"});
+          //
+          // collectionsRef.insert(dataVar, function(err, result){
+          //   console.log("inserted successfully!!!");
+          // });
+
+          //update data into MongoDB
+          // collectionsRef.update({userId:"0001"}, {address:"chennai-100"}, function(err){
+          //   if(!err)
+          //   console.log(err)
+          // });
+
+          //Remove Data from MongoDB
+          // collectionsRef.remove({"userId":"0005"}, 1);
+
+          //Show from MongoDB collections
+          // collectionsRef.find({}, function(err, cursor){
+          //   cursor.toArray(function(err, data){
+          //     console.log(data)
+          //   });
+          // });
+      }
+    });
+
+    // mongo.connect(databaseUrl, collections, function(err, dbn){
+    // });
+
+    app.use(serveStatic(__dirname+"/app")).listen(9000, function(){
+      console.log("Node server running at 9000");
     });
