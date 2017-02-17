fa499c28d6b7ddda7f3b41a2bed09a031ad08e61
siva <sivaramalingam.r@whatarage.com>
2017-02-17 10:07:24 +0530

    Remainder app updated on 17/02/17

--- app/assets/scripts/script.js
+++ app/assets/scripts/script.js
@@ -4,11 +4,44 @@
       remainderApp.controller("login", function($scope){
 
       });
-    // register controller
+      // register controller
       remainderApp.controller("registeration", function($scope){
         $scope.submitForm = function(isValid){
           if(isValid)
             console.log("form submitted successfully!!")
         }
       });
+
+      remainderApp.controller("moviesController", function($scope){
+        $scope.movies = ["XXX", "YYY", "ZZZ", "WWW"];
+      });
+
+      remainderApp.directive("movieName", function(){
+        return{
+          restrict: "A",
+          transclude: 'true',
+          template: "<span ng-transclude></span>",
+          link: function(scope, element, attributs){
+            console.log(attributs.title)
+          }
+        }
+      });
+
+      remainderApp.directive("ngCity", function(){
+        return{
+          restrict: "A",
+          require: "ngModel",
+          template: "<h1>You have entered city name: {{city}}</h1>",
+          compile: function(element, attributs){
+
+          }
+        }
+      });
+
+      // remainderApp.directive("spanClass", function(){
+      //   return{
+      //     restrict: "EA",
+      //     template: "<div>Welcome to custom directive</div>"
+      //   }
+      // });
 })();
