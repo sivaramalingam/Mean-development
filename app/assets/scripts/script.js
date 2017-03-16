9a9d0bc65b7e2d28dd6b68f87960942e7252fe79
siva <sivaramalingam.r@whatarage.com>
2017-03-16 18:23:18 +0530

    Updates on 16/03/17

--- app/assets/scripts/script.js
+++ app/assets/scripts/script.js
@@ -1,19 +1,78 @@
 (function(){
-  var remainderApp = angular.module("remainder",[]);
-      //login controller
-      remainderApp.controller("login", function($scope){
+  var remainderApp = angular.module("remainder", ['ngRoute']);
+      //Routes
+      remainderApp.config(['$routeProvider',function($routeProvider){
+        $routeProvider.
+          when("/login", {
+            templateUrl: 'templates/login.htm',
+            controller: 'loginController',
+            reload: true
+          }).
+          when("/register", {
+            templateUrl: 'templates/register.htm',
+            controller: "registerationController",
+            reload: true
+          }).
+          otherwise({
+            redirectTo: "index.htm"
+          });
+      }]);
 
+      //set rootscope in module
+      remainderApp.run(function($rootScope){
+        $rootScope.visible = true;
       });
+
+      //login controller
+      remainderApp.controller("loginController", function($scope, $rootScope){
+        $rootScope.visible = false;
+        $scope.msg = "Hi, Welcome to login page, please give your username and password below."
+      });
+
       // register controller
-      remainderApp.controller("registeration", function($scope){
+      remainderApp.controller("registerationController", function($scope, $rootScope){
+        $rootScope.visible = false;
+
         $scope.submitForm = function(isValid){
           if(isValid)
             console.log("form submitted successfully!!")
         }
+        // $scope.helloServ = firstService.helloService();
+        // $scope.sumServ = firstService.sumService();
+        // $scope.firstfact = firstfactory;
+      });
+
+      //remainder controller
+      remainderApp.controller("remainderController", function($scope, $rootScope){
+         $rootScope.visible = true;
+        //
+        // $scope.clickEvent = function(){
+        //   alert();
+        // }
       });
 
       remainderApp.controller("moviesController", function($scope){
         $scope.movies = ["XXX", "YYY", "ZZZ", "WWW"];
+      });
+
+      remainderApp.service("firstService", function(){
+        this.helloService = function(){
+          return "hello world!!!";
+        }
+        this.sumService = function(){
+          var x = 10;
+          return x + 25;
+        }
+      });
+
+      remainderApp.factory("firstfactory", function(){
+        var x = {}, sum = 0;
+        x.addSum = function(){
+          var y= 15;
+            sum = y+10;
+            return sum;
+        }
+        return x.addSum();
       });
 
       remainderApp.directive("movieName", function(){
